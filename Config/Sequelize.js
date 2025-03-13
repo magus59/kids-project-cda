740a7e7bf6278efc65485fc518f66ba61ee9d29a
yahia <100646468+magus59@users.noreply.github.com>
2025-03-13 13:30:55 +0100

    modification du fichier .env

--- Config/Sequelize.js
+++ Config/Sequelize.js
@@ -5,9 +5,9 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-    port: process.env.DB_PORT,
+    host: process.env.DB_HOST || 'localhost',
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
   }
 );
 
